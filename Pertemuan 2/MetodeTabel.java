cc4e4eb0a5817e19a796e9ecac6eaa320da39330
Maya Amanda <mayaamandaG@gmail.com>
2021-01-04 19:49:43 +0700

    Pertemuan 3 sampai Pertemuan 11

--- Pertemuan 2/MetodeTabel.java
+++ Pertemuan 2/MetodeTabel.java
@@ -1,39 +1,49 @@
-import java.util.*;
+import java.util.Scanner;
+/*
+    SOAL
+    Batas Bawah : 1
+    Batas Atas : 3
+    Iterasi Maks : 3
 
-public class MetodeTabel {
-    public static double y(double x) {
-        double nilai= (x*x*x) - (2*x*x) - x + 1;
-        return nilai;
+    Hasil : 
+    Akar diantara 1.6666666666666665 dan 2.333333333333333
+    Akar lebih dekat ke x[2] = 2.333333333333333
+*/
+
+public class MetodeTabel{
+    public static double y(float x) {
+        double hasil= Math.pow(x, 2) - 2 * Math.pow(x, 2) - x + 1;
+        return hasil;
     }
-    public static void main(String[] args) {
+    public static void main(String args[]){
+        Scanner input= new Scanner(System.in);
 
-        MetodeTabel uji = new MetodeTabel();
-        Scanner input = new Scanner(System.in);
-        System.out.print("masukan batas Bawah (Xbawah) : ");
-        double xBawah = input.nextDouble();
-        System.out.print("masukan batas Atas (Xatas) : ");
-        double xAtas = input.nextDouble();
-        System.out.print("masukan jumlah pembagian (N) : ");
-        int n = input.nextInt();
+        System.out.print("Batas bawah(Xbawah) : ");
+        double xBawah= input.nextDouble();
+        System.out.print("Batas atas(Xatas) : ");
+        double xAtas= input.nextDouble();
+        System.out.print("Jumlah pembagian(N) : ");
+        int n= input.nextInt();
+
+        double h= (xAtas-xBawah)/n;
         
-        double h = (xAtas - xBawah) / n;
+        double[] x= new double [n+1];
+        double[] y= new double [n+1];
 
-        double x[] = new double[n + 1];
-        double y[] = new double[n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            x[i] = xBawah + (i * h);
-            y[i]= y(x[i]);
-            System.out.println("nilai x[" +i+ "] = " + x[i] + " & nilai y[" +i+ "] = " + y[i]);
+        for(int i=0; i<=n; i++){
+            x[i]= i * h + xBawah;
+            y[i]= y(-x[i]);
+            System.out.println("x[" +i+ "] = " +x[i]+ "\ty[" +i+ "] = " +y[i]);
         }
 
-        for (int j=0;j<=(n-1);j++) {
-            if(y[j] * y[j+1] < 0) {
-                System.out.println(" " + x[j] + " dan " + x[j + 1]);
-                if (Math.abs (y[j]) < Math.abs(y[j+1])) {
-                    System.out.println("Akar lebih dekat ke " + x[j]);
-                } else {
-                    System.out.println("Akar lebih dekat ke  " + x[j+1]);
+        for(int j=0; j<n; j++){
+            if(y[j]*y[j+1]<0){
+                System.out.println("Akar diantara " +x[j]+ " dan " +x[j+1]);
+                if(Math.abs(y[j]) < Math.abs(y[j + 1])){
+                    System.out.println("Akar lebih dekat ke x[" +j+ "] = " +x[j]);
+                }
+                else{
+                    System.out.println("Akar lebih dekat ke x[" +(j+1)+ "] = " +x[j+1]);
                 }
             }
         }
